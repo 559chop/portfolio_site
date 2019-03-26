68809410204c8e0ad79fb649ed2ce763e81b0ab1
Thien Nguyen <Thiennguyenwsb@gmail.com>
2019-03-25 17:44:47 -0700

    Updates

---
+++ precache-manifest.2306064fe7a2b6d22e2f9adf0154a83d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d86dca4304ffb717e06e0b86ac9ead55",
+    "url": "/portfolio_site/static/media/quotes.d86dca43.jpg"
+  },
+  {
+    "revision": "c466973581ad83183d2d87001db26acf",
+    "url": "/portfolio_site/static/media/portfolio.c4669735.jpg"
+  },
+  {
+    "revision": "e2f020fcaa725a2909a235447eaa0513",
+    "url": "/portfolio_site/static/media/father_daughter.e2f020fc.png"
+  },
+  {
+    "revision": "f10d57ccedbbc6240a63",
+    "url": "/portfolio_site/static/js/runtime~main.f10d57cc.js"
+  },
+  {
+    "revision": "d7424eaa73a5ed5d9f86",
+    "url": "/portfolio_site/static/js/main.d7424eaa.chunk.js"
+  },
+  {
+    "revision": "6e19783be14b68e43ead",
+    "url": "/portfolio_site/static/js/2.6e19783b.chunk.js"
+  },
+  {
+    "revision": "eccafab949d83f6908ec0e9e5416bd7e",
+    "url": "/portfolio_site/index.html"
+  }
+];
